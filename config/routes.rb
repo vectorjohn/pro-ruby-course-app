969bb6a4b3f0eb4aa64eb838f3fd5f9b9fc1569e
John Reeves <jreeves@k12.com>
2018-12-18 16:16:53 -0800

    I can make em and show em and flash em

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "pages#home"
+  get "/about", to: "pages#about"
+
+  resources :todos do
+      get 'comments'
+      get 'fart'
+      put 'fart'
+  end
 end
